e371928507fe10e44c7e9fc94d1f2ec361dd815f
Aman <amanfarhan2008@gmail.com>
2025-07-14 02:10:55 +0500

    prectice loop

--- app.js
+++ app.js
@@ -41,12 +41,109 @@
 // console.log("loop was endeed");
 
 
+// for loop :
 
-let sum = 0;
-for(let i = 1 ; i <= 10; i++){
-    sum = sum + i 
-    console.log(i);  
-};
 
-console.log(sum);
+// let a = 5;
+// for(let i = 1 ; i <= 10; i++){
+//     console.log(a + " x " + i + " = " + a*i);
+// }
+
+
+
+// let sum = 0;
+// for(let i = 1 ; i <= 10; i++){
+//     sum = sum + i 
+//     console.log(i);  
+// };
+
+// console.log(sum);
+
+
+
+// let sum = +prompt("enter a num ")
+// for(let i = 1 ; i <= 5 ; i++){
+//     sum = sum + i;
+//     document.write(sum + "<br>");
+//     // document.write(sum + 5);
+// }
+// console.log(sum);
+// console.log("5");
+
+
+// while loop :
+
+
+// let i = 1;
+// let a = 5;
+// while(i <= 10){
+//     console.log(a +" x " + i + " = "+ a*i);
+//     i++
+// }
+
+
+
+// let i = 1;
+// let sum = 1;
+// while (i <= 5){
+//     sum = sum + i ;
+//     console.log("i = " + i );
+//     i++
+// }
+// console.log(sum)
+
+
+// let i = 1;
+// let a = prompt("enter number ")
+// while(i <= 10){
+//     console.log(a +" x "+ i + " = " + a*i );
+//     i++
+// }
+
+
+// do while loop :
+
+
+// let i = 0 ;
+// do {
+//     console.log("i = "+ i );
+//     i++
+// } while (i <= 20)
+
+
+// let i = 0;
+// let sum = i;
+// let a = 5;
+
+// do {
+//     sum = sum + i
+//     console.log(a + " x " + i + " = " + a*i);
+//     console.log(sum);
+//     i++
+// } while (i <=20 );
+
+
+// let a = +prompt("Enter a Table Number:");
+// let b = 1
+// let c = 1
+
+// do {
+//     c = c + b;
+//     console.log(a + " x " + b + " = " + a*b);
+//     b++
+// } while (b <= 20);
+// console.log(c);
+
+
+
+// let i = 1 ;
+// let a = 5 ;
+
+// do {
+//     console.log(a + " x " + i + " = " + a * i );
+//     i++
+// } while (i <= 10 );
+
+
+
 
